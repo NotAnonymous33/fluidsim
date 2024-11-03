07e4f04638cc53416c80a48ef55b3e2a23b39600
Ismail <ismail.choudhury@student.manchester.ac.uk>
2024-11-03 00:35:07 +0000

    pygame init

--- main.py
+++ main.py
@@ -1,2 +1,36 @@
-print("hello world")
-print("Hello2")
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((720, 720))
+clock = pygame.time.Clock()
+running = True
+
+
+x = -100
+speed = 300
+fps = 200
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("black")
+
+    # RENDER YOUR GAME HERE
+    # draw a circle
+    pygame.draw.circle(screen, "red", (x, 360), 100)
+    x += speed / fps
+    if x > 820:
+        x = -100
+    
+    
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(fps)  # limits FPS to 60
+
+pygame.quit()
